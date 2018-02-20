356a7d57bcebe4b39fd13a1d3f6d380e4797cf50
Joonas Westlin <joonas.westlin@gmail.com>
2018-02-20 20:48:11 +0200

    Token cache now uses IMemoryCache if possible, and IDistributedCache after that.
    It prevents the app from hammering e.g. a Redis instance with requests when it is unnecessary.

--- Core2AadAuth/Services/AdalDistributedTokenCache.cs
+++ Core2AadAuth/Services/AdalDistributedTokenCache.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace Core2AadAuth.Services
@@ -10,19 +11,22 @@ namespace Core2AadAuth.Services
     /// </summary>
     public class AdalDistributedTokenCache : TokenCache
     {
-        private readonly IDistributedCache _cache;
+        private readonly IDistributedCache _distributedCache;
+        private readonly IMemoryCache _memoryCache;
         private readonly IDataProtector _dataProtector;
         private readonly string _userId;
 
         /// <summary>
         /// Constructs a token cache
         /// </summary>
-        /// <param name="cache">Distributed cache to use to store data</param>
+        /// <param name="distributedCache">Distributed cache used as level 2 cache</param>
+        /// <param name="memoryCache">Memory cache used as level 1 cache</param>
         /// <param name="dataProtectionProvider">The protector provider for encrypting/decrypting the cached data</param>
         /// <param name="userId">The user's unique identifier</param>
-        public AdalDistributedTokenCache(IDistributedCache cache, IDataProtectionProvider dataProtectionProvider, string userId)
+        public AdalDistributedTokenCache(IDistributedCache distributedCache, IMemoryCache memoryCache, IDataProtectionProvider dataProtectionProvider, string userId)
         {
-            _cache = cache;
+            _distributedCache = distributedCache;
+            _memoryCache = memoryCache;
             _dataProtector = dataProtectionProvider.CreateProtector("AadTokens");
             _userId = userId;
             BeforeAccess = BeforeAccessNotification;
@@ -34,7 +38,8 @@ namespace Core2AadAuth.Services
             //Called before ADAL tries to access the cache,
             //so this is where we should read from the distibruted cache
             //It sucks that ADAL's API is synchronous, so we must do a blocking call here
-            byte[] cachedData = _cache.Get(GetCacheKey());
+            byte[] cachedData = GetFromCaches();
+
             if (cachedData != null)
             {
                 //Decrypt and deserialize the cached data
@@ -47,6 +52,29 @@ namespace Core2AadAuth.Services
             }
         }
 
+        private byte[] GetFromCaches()
+        {
+            byte[] cachedData;
+            string cacheKey = GetCacheKey();
+            //Try Level 1, in-memory cache first
+            if (!_memoryCache.TryGetValue(cacheKey, out cachedData))
+            {
+                //If that doesn't work, then try distributed cache
+                cachedData = _distributedCache.Get(GetCacheKey());
+                if (cachedData != null)
+                {
+                    //Found from L2, write to L1 for next access
+                    SetToMemoryCache(cachedData);
+                }
+            }
+            return cachedData;
+        }
+
+        private void SetToMemoryCache(byte[] data)
+        {
+            _memoryCache.Set(GetCacheKey(), data, TimeSpan.FromHours(8));
+        }
+
         private void AfterAccessNotification(TokenCacheNotificationArgs args)
         {
             //Called after ADAL is done accessing the token cache
@@ -54,10 +82,14 @@ namespace Core2AadAuth.Services
             {
                 //In this case the cache state has changed, maybe a new token was written
                 //So we encrypt and write the data to the distributed cache
-                _cache.Set(GetCacheKey(), _dataProtector.Protect(Serialize()), new DistributedCacheEntryOptions
+                var data = _dataProtector.Protect(Serialize());
+                //Put data to both caches, so that 1) it is available right away in-memory here
+                //and 2) in L2 for other instances
+                _distributedCache.Set(GetCacheKey(), data, new DistributedCacheEntryOptions
                 {
                     AbsoluteExpirationRelativeToNow = TimeSpan.FromDays(1)
                 });
+                SetToMemoryCache(data);
                 HasStateChanged = false;
             }
         }
--- Core2AadAuth/Services/TokenCacheFactory.cs
+++ Core2AadAuth/Services/TokenCacheFactory.cs
@@ -2,6 +2,7 @@
 using Core2AadAuth.Extensions;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace Core2AadAuth.Services
@@ -12,18 +13,21 @@ namespace Core2AadAuth.Services
     public class TokenCacheFactory : ITokenCacheFactory
     {
         private readonly IDistributedCache _distributedCache;
+        private readonly IMemoryCache _memoryCache;
         private readonly IDataProtectionProvider _dataProtectionProvider;
 
-        public TokenCacheFactory(IDistributedCache distributedCache, IDataProtectionProvider dataProtectionProvider)
+        public TokenCacheFactory(IDistributedCache distributedCache, IMemoryCache memoryCache, IDataProtectionProvider dataProtectionProvider)
         {
             _distributedCache = distributedCache;
+            _memoryCache = memoryCache;
             _dataProtectionProvider = dataProtectionProvider;
         }
 
         public TokenCache CreateForUser(ClaimsPrincipal user)
         {
             string userId = user.GetObjectId();
-            return new AdalDistributedTokenCache(_distributedCache, _dataProtectionProvider, userId);
+            return new AdalDistributedTokenCache(
+                _distributedCache, _memoryCache, _dataProtectionProvider, userId);
         }
     }
 }
